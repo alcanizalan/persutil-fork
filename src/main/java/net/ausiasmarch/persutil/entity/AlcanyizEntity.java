9e824fde957ea29dd9b1669bbc707b66c3beeb61
Alan <alanalcaniz24@gmail.com>
2025-11-28 16:25:31 +0100

    terminado

--- src/main/java/net/ausiasmarch/persutil/entity/AlcanyizEntity.java
+++ src/main/java/net/ausiasmarch/persutil/entity/AlcanyizEntity.java
@@ -18,7 +18,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "jsquestions")
+@Table(name = "jspreguntas")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -32,20 +32,20 @@ public class AlcanyizEntity{
     @Size(min = 3, max = 1024)
     private String question;
     @NotNull
-    @Size(min = 3, max = 1024)
+    @Size(max = 1024)
     private String answer1;
     @NotNull
-    @Size(min = 3, max = 1024)
+    @Size(max = 1024)
     private String answer2;
     @NotNull
-    @Size(min = 3, max = 1024)
+    @Size(max = 1024)
     private String answer3;
     @NotNull
-    @Size(min = 3, max = 1024)
+    @Size(max = 1024)
     private String answer4;
     @NotNull
     private long correct;
-    @NotNull
+    @Nullable
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private LocalDateTime create_date;
     @Nullable
